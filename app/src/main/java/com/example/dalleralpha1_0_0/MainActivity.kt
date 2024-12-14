3aa1d05bc80aed2b2102c06c66001051098fa929
YiChi <chi20021225@gmail.com>
2024-12-14 19:50:03 +0800

    監控登入跟註冊按鈕不能為空

--- app/src/main/java/com/example/dalleralpha1_0_0/MainActivity.kt
+++ app/src/main/java/com/example/dalleralpha1_0_0/MainActivity.kt
@@ -45,42 +45,49 @@ class MainActivity : AppCompatActivity() {
         }
 
         login.setOnClickListener{
-            //打api的時候再打開
-            Api.retrofitService.login(LoginRequest(ac.text.toString(),pw.text.toString())).enqueue(object:retrofit2.Callback<LoginResponse>{
-                override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>)
-                {
-                    if (response.isSuccessful){
-                        when (response.body()?.success) {
-                            "true" -> {
-                                val token = response.body()?.message ?: ""
-                                tokenManager.saveToken(token) // 保存 Token
-                                Toast.makeText(this@MainActivity,"登入成功",Toast.LENGTH_SHORT).show()
-                                Log.d("success!", response.body().toString())
-                                //僅跳頁，尚未傳值
-                                val intent=Intent()
-                                intent.setClass(this@MainActivity,MenuActivity::class.java)
-                                startActivity(intent)
+            when {
+                ac.text.toString().trim().isBlank() && pw.text.toString().trim().isBlank() -> Toast.makeText(this@MainActivity,"請輸入帳密", Toast.LENGTH_SHORT).show()
+                ac.text.toString().trim().isBlank() -> Toast.makeText(this@MainActivity,"請輸入帳號", Toast.LENGTH_SHORT).show()
+                pw.text.toString().trim().isBlank() -> Toast.makeText(this@MainActivity,"請輸入密碼", Toast.LENGTH_SHORT).show()
+                else -> {
+                    //登入api
+                    Api.retrofitService.login(LoginRequest(ac.text.toString(),pw.text.toString())).enqueue(object:retrofit2.Callback<LoginResponse>{
+                        override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>)
+                        {
+                            if (response.isSuccessful){
+                                when (response.body()?.success) {
+                                    "true" -> {
+                                        val token = response.body()?.message ?: ""
+                                        tokenManager.saveToken(token) // 保存 Token
+                                        Toast.makeText(this@MainActivity,"登入成功",Toast.LENGTH_SHORT).show()
+                                        Log.d("success!", response.body().toString())
+                                        //僅跳頁，尚未傳值
+                                        val intent=Intent()
+                                        intent.setClass(this@MainActivity,MenuActivity::class.java)
+                                        startActivity(intent)
+                                    }
+                                    "false" ->{
+                                        Toast.makeText(this@MainActivity,"帳密錯誤，登入失敗",Toast.LENGTH_SHORT).show()
+                                        Log.d("success!", response.body().toString())
+                                    }else->{
+                                    Log.d("failure", "Unknown success value: " + response.body()?.success)
+                                }
+                                }
                             }
-                            "false" ->{
-                                Toast.makeText(this@MainActivity,"帳密錯誤，登入失敗",Toast.LENGTH_SHORT).show()
+                            else{
+                                Toast.makeText(this@MainActivity,"發生未預期的錯誤",Toast.LENGTH_SHORT).show()
                                 Log.d("success!", response.body().toString())
-                            }else->{
-                                Log.d("failure", "Unknown success value: " + response.body()?.success)
                             }
                         }
-                    }
-                    else{
-                        Toast.makeText(this@MainActivity,"發生未預期的錯誤",Toast.LENGTH_SHORT).show()
-                        Log.d("success!", response.body().toString())
-                    }
-                }
 
-                override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-                    Toast.makeText(this@MainActivity,"Something went wrong $t",Toast.LENGTH_SHORT).show()
-                    Log.e("fail",t.toString())
-                }
+                        override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                            Toast.makeText(this@MainActivity,"Something went wrong $t",Toast.LENGTH_SHORT).show()
+                            Log.e("fail",t.toString())
+                        }
 
-            })
+                    })
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/dalleralpha1_0_0/RegisterActivity.kt
+++ app/src/main/java/com/example/dalleralpha1_0_0/RegisterActivity.kt
@@ -23,7 +23,7 @@ class RegisterActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        //連結登入畫面的EditText
+        //連結註冊畫面的EditText
         val name =findViewById<EditText>(R.id.name_content)
         val ac =findViewById<EditText>(R.id.ac_content)
         val pw =findViewById<EditText>(R.id.pw_content)
@@ -32,40 +32,53 @@ class RegisterActivity : AppCompatActivity(){
         val back = findViewById<Button>(R.id.backtologin)
 
         register.setOnClickListener{
-            //打api的時候再打開
-            Api.registerService.register(RegisterRequest(ac.text.toString(),name.text.toString(),pw.text.toString())).enqueue(object:retrofit2.Callback<RegisterResponse>{
-                override fun onResponse(call: Call<RegisterResponse>, response: Response<RegisterResponse>)
-                {
-                    if (response.isSuccessful){
-                        when (response.body()?.success) {
-                            "true" -> {
-                                Toast.makeText(this@RegisterActivity,"註冊成功", Toast.LENGTH_SHORT).show()
-                                Log.d("success!", response.body().toString())
-                                //僅跳頁，尚未傳值
-                                val intent= Intent()
-                                intent.setClass(this@RegisterActivity,MainActivity::class.java)
-                                startActivity(intent)
+            when {
+                // 全部為空
+                name.text.toString().trim().isBlank() && ac.text.toString().trim().isBlank() && pw.text.toString().trim().isBlank() ->
+                    Toast.makeText(this@RegisterActivity, "請輸入註冊資訊", Toast.LENGTH_SHORT).show()
+                // 任一必要資訊為空
+                name.text.toString().trim().isBlank() ->
+                    Toast.makeText(this@RegisterActivity, "請輸入名字", Toast.LENGTH_SHORT).show()
+                ac.text.toString().trim().isBlank() ->
+                    Toast.makeText(this@RegisterActivity, "請輸入帳號", Toast.LENGTH_SHORT).show()
+                pw.text.toString().trim().isBlank() ->
+                    Toast.makeText(this@RegisterActivity, "請輸入密碼", Toast.LENGTH_SHORT).show()
+                else -> {
+                    Api.registerService.register(RegisterRequest(ac.text.toString(),name.text.toString(),pw.text.toString())).enqueue(object:retrofit2.Callback<RegisterResponse>{
+                        override fun onResponse(call: Call<RegisterResponse>, response: Response<RegisterResponse>)
+                        {
+                            if (response.isSuccessful){
+                                when (response.body()?.success) {
+                                    "true" -> {
+                                        Toast.makeText(this@RegisterActivity,"註冊成功", Toast.LENGTH_SHORT).show()
+                                        Log.d("success!", response.body().toString())
+                                        //僅跳頁，尚未傳值
+                                        val intent= Intent()
+                                        intent.setClass(this@RegisterActivity,MainActivity::class.java)
+                                        startActivity(intent)
+                                    }
+                                    "false" ->{
+                                        Toast.makeText(this@RegisterActivity,"註冊失敗", Toast.LENGTH_SHORT).show()
+                                        Log.d("success!", response.body().toString())
+                                    }else->{
+                                    Log.d("failure", "Unknown success value: " + response.body()?.success)
+                                }
+                                }
                             }
-                            "false" ->{
-                                Toast.makeText(this@RegisterActivity,"註冊失敗", Toast.LENGTH_SHORT).show()
+                            else{
+                                Toast.makeText(this@RegisterActivity,"此帳號已存在", Toast.LENGTH_SHORT).show()
                                 Log.d("success!", response.body().toString())
-                            }else->{
-                            Log.d("failure", "Unknown success value: " + response.body()?.success)
+                            }
                         }
+
+                        override fun onFailure(call: Call<RegisterResponse>, t: Throwable) {
+                            Toast.makeText(this@RegisterActivity,"Something went wrong $t", Toast.LENGTH_SHORT).show()
+                            Log.e("fail",t.toString())
                         }
-                    }
-                    else{
-                        Toast.makeText(this@RegisterActivity,"發生未預期的錯誤", Toast.LENGTH_SHORT).show()
-                        Log.d("success!", response.body().toString())
-                    }
-                }
 
-                override fun onFailure(call: Call<RegisterResponse>, t: Throwable) {
-                    Toast.makeText(this@RegisterActivity,"Something went wrong $t", Toast.LENGTH_SHORT).show()
-                    Log.e("fail",t.toString())
+                    })
                 }
-
-            })
+            }
         }
         back.setOnClickListener{
             val intent= Intent()
